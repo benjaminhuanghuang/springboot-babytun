4ede8f17d289edaf4674fc99d57fca74bc7d3627
benjaminhuanghuang <bhuang@rms.com>
2020-09-15 19:03:29 -0700

    add project

---
+++ seckill-sample/src/test/java/ben/study/seckillsample/SeckillSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ben.study.seckillsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
